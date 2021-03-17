d3b1c4d5eea7eceb0c190726bef1fe60c4366a88
Enoch Kan <kanxx030@gmail.com>
2021-03-17 00:53:31 +0000

    added logic, too tired going to bed now

--- other/rotate_matrix.py
+++ other/rotate_matrix.py
@@ -1,11 +1,18 @@
-class RotatingMatrix:
+class RotatingMatrix90:
     import numpy as np
 
-    def __init__(self, matrix, angle):
+    def __init__(self, matrix):
         self.matrix = matrix
-        self.angle = angle
+        self.is_square = False
 
     def rotate(self):
+        w, h = self.matrix.shape[0], self.matrix.shape[1]
+        if w == h:
+            self.is_square = True
+            """0,0 => 0,1; 0,1 => 1,1; 1,1 => 1,0; 1,0 => 0,0;"""
+            tmat = # insert transformation matrix here
+            # do matmul
+
         return np.array([[4.0, 1.0], [2.0, 3.0]])
 
 
@@ -14,7 +21,7 @@ if __name__ == "__main__":
     from numpy.testing import assert_array_equal
 
     matrix = np.array([[1.0, 2.0], [3.0, 4.0]])
-    rm = RotatingMatrix(matrix=matrix, angle=90)
+    rm = RotatingMatrix90(matrix=matrix)
     result = rm.rotate()
     # correct output should be np.array([[4.0, 1.0], [2.0, 3.0]])
     assert_array_equal(result, np.array([[4.0, 1.0], [2.0, 3.0]]))
